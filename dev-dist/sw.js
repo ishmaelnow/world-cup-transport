68ccd7e7b7636e174daf07e2cc3bcf90c70efeee
Ishmael <ishmaelkosh@gmail.com>
2026-06-26 15:56:42 -0500

    Update Codemagic Android build configuration

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.flpg2vjk8qg"
+    "revision": "0.547rkg4v8lg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
